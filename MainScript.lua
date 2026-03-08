180a2e3232d2a8b9c2397b3025cc832d8c5647ad
totemoflol <totemoflol4@gmail.com>
2026-03-08 14:28:27 +0800

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,7 +1,7 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 -- Window
 local Window = Rayfield:CreateWindow({
-   Name = "Potato Script V1.66",
+   Name = "Potato Script V1.67",
    Icon = "venetian-mask", -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
    LoadingTitle = "Idle Potato Game (T)",
    LoadingSubtitle = "by Totemoflol",
@@ -459,7 +459,7 @@ local MacroV1Toggle = MacroTab:CreateToggle({
 	            "transcendent_harvest"
                 }
                 game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("PurchaseClickUpgrade"):FireServer(unpack(args))
-                task.wait(3)
+                task.wait(4)
                
                -- 27.83B Sell And Harvest 2 (12)
                 game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("SellGoldenPotatoes"):FireServer(27_830_000_000)
