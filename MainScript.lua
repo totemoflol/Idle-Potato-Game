bcac80b672d38faa877a9962e3a20af2358a6575
totemoflol <totemoflol4@gmail.com>
2026-03-16 23:57:56 +0800

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -22,12 +22,12 @@ s1.SoundId = "rbxassetid://5793681247"
 s1.Volume = 1
 s1:Play()
 
-Version = "V2.90"
+Version = "V2.92"
 print(Version)
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 -- Window
 local Window = Rayfield:CreateWindow({
-   Name = "Potato Script V2.90",
+   Name = "Potato Script V2.92",
    Icon = "venetian-mask", -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
    LoadingTitle = "Idle Potato Game (T)",
    LoadingSubtitle = "by Totemoflol",
