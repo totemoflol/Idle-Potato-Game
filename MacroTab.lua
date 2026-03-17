8c197bf00f7f0cad1b82bf9cbfb27b034bcf57ef
totemoflol <totemoflol4@gmail.com>
2026-03-17 12:28:43 +0800

    Update MacroTab.lua

--- MacroTab.lua
+++ MacroTab.lua
@@ -265,7 +265,7 @@ local GeneratorMacroV1Toggle = MacroTab:CreateToggle({
                -- Temporal Harvester 1
                 local args = {"temporal_harvester"}
                 game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("PurchaseGenerator"):FireServer(unpack(args))
-                task.wait(3)
+                task.wait(3.2)
 
                -- 100B Sell
                 game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("SellGoldenPotatoes"):FireServer(100_000_000_000)
@@ -280,4 +280,4 @@ local GeneratorMacroV1Toggle = MacroTab:CreateToggle({
         end)
     end,
 })
-print("Macro Tab Loaded")
+print("Macro Tab Loaded V1.02")
