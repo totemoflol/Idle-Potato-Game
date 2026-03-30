8b4930cd87e72a5f666dba36dba15d1bc407eca5
ksjx67 <kahshing222@gmail.com>
2026-03-30 14:14:56 +0800

    Create Admin.lua

---
+++ Admin.lua
@@ -0,0 +1,2 @@
+local AdminTab = Window:CreateTab("Admin", "gem") -- Title, Image
+local AdminFeaturesLabel = AdminTab:CreateLabel("Premium Features Fahhhh", 4483362458, Color3.fromRGB(255, 255, 255), false) -- Title, Icon, Color, IgnoreTheme
