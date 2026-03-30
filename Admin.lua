339d18fe8cb132f4ed7d80319234dea53f80daf6
totemoflol <totemoflol4@gmail.com>
2026-03-30 17:57:27 +0800

    Update Admin.lua

--- Admin.lua
+++ Admin.lua
@@ -1,5 +1,5 @@
-local AdminTab = Window:CreateTab("Admin", "gem") -- Title, Image
-local AdminFeaturesLabel = AdminTab:CreateLabel("Premium", 4483362458, Color3.fromRGB(255, 255, 255), false) -- Title, Icon, Color, IgnoreTheme
+local PremiumTab = Window:CreateTab("Premium", "gem") -- Title, Image
+local AdminFeaturesLabel = PremiumTab:CreateLabel("Premium", 4483362458, Color3.fromRGB(255, 255, 255), false) -- Title, Icon, Color, IgnoreTheme
 
 local player = game:GetService("Players").LocalPlayer
 local AllowedTaters = getgenv().whitelistedtaters or {}
@@ -7,7 +7,7 @@ local AllowedTaters = getgenv().whitelistedtaters or {}
 if AllowedTaters[player.UserId] then
     local EmojiBuying = false
 
-    ShopTab:CreateToggle({
+    PremiumTab:CreateToggle({
         Name = "Emoji Mystery Potato Auto Buy",
         CurrentValue = false,
         Flag = "EmojiBuy",
