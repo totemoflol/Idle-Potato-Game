ede0e8b94220c9865204b4d4827da4152b41ae6c
ksjx67 <kahshing222@gmail.com>
2026-03-30 14:18:10 +0800

    Update ShopTab.lua

--- ShopTab.lua
+++ ShopTab.lua
@@ -90,36 +90,4 @@ local ClickAutoBuyToggle = ShopTab:CreateToggle({
    end,
 })
 
-local MysteryPotatoDivider = ShopTab:CreateDivider()
-
-local player = game:GetService("Players").LocalPlayer
-local AllowedTaters = getgenv().whitelistedtaters or {}
-
-if AllowedTaters[player.UserId] then
-    local EmojiBuying = false
-
-    ShopTab:CreateToggle({
-        Name = "Emoji Mystery Potato Auto Buy",
-        CurrentValue = false,
-        Flag = "EmojiBuy",
-        Callback = function(EmojiAutoBuy)
-            EmojiBuying = EmojiAutoBuy
-
-            if EmojiBuying then
-                task.spawn(function()
-                    while EmojiBuying do
-                        game:GetService("ReplicatedStorage")
-                            :WaitForChild("Remotes")
-                            :WaitForChild("PurchaseShopPotato")
-                            :FireServer("emoji_mystery_potato")
-
-                        task.wait(60)
-                    end
-                end)
-            end
-        end,
-    })
-end
-      
-
 print("Shop Tab Loaded V1.10")
